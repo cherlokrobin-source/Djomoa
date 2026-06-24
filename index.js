b0a2371a00db11f997162330e1ad9e7a0eb332cf
Nemimeche benaissa <cherlokrobin@gmail.com>
2026-06-24 14:16:45 +0100

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-import { startDjomoaProcess } from './engine/engine_albab.js';
-import events from './mustawda/events_db.json' assert { type: 'json' };
-
-// البدء بضخ البيانات في المحرك
-startDjomoaProcess(events);
-
